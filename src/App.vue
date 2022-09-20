1e393a5be435b8556292aa9c7c81cf43788b508e
KELVIN MELIM <kelvin.melim@alunos.sc.senac.br>
2022-09-19 21:38:56 -0300

    1 commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="date" v-model="data">
+        <data-comp :dataParam="data"> </data-comp>
+    </div>
+    
+</template>
+
+<script>
+import DataComp from './components/dataComp.vue'
+
+export default {
+  data(){
+    return{
+      data: null
+    }
+  },
+
+  components: {
+    DataComp
+  },
+
+  computed: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/dataComp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        Resultado     
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+      dataParam: String,
+    },
+  methods: {
+    data() {
+      return {
+        mes: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun',
+        'jul', 'ago', 'set', 'out', 'nov', 'dez']
+      }
+    },
+    getExtenso(){
+      if (this.dataParam) {
+        const d = this.dataParam.split('-')
+        return d[2] + ' ' + this.mes[parseInt(d[1]-1)]
+                    + ' ' + d[0]
+      }
+    }
+  }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
